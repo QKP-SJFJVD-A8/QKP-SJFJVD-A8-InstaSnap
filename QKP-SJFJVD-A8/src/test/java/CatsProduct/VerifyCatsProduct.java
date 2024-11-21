27f8932512770a647beb753e274555c278fdac5c
srini <srini@KAAL>
2024-11-21 16:14:51 +0530

    Cats product created

---
+++ QKP-SJFJVD-A8/src/test/java/CatsProduct/VerifyCatsProduct.java
@@ -0,0 +1,17 @@
+package CatsProduct;
+
+import org.testng.annotations.Test;
+
+import com.Ecommerce.InstaSnap.GenericLibrary.BaseTest;
+
+public class VerifyCatsProduct extends BaseTest {
+	@Test
+	public void addProduct() {
+		
+	}
+	
+	public void removeProduct() {
+		
+	}
+
+}
