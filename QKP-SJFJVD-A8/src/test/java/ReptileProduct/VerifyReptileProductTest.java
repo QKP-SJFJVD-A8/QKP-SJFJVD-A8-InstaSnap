3d0e1f5aa47185d5f18a5649f3dcbd5f4e43a368
pavan <pavan@192.168.0.9>
2024-11-29 19:22:48 +0530

    Test Cases Completed - Reptiles- Team B

--- QKP-SJFJVD-A8/src/test/java/ReptileProduct/VerifyReptileProductTest.java
+++ QKP-SJFJVD-A8/src/test/java/ReptileProduct/VerifyReptileProductTest.java
@@ -2,21 +2,14 @@ package ReptileProduct;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.io.FileHandler;
 import org.testng.Reporter;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 import com.GenericLibrary.BaseTest;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class VerifyReptileProductTest extends BaseTest {
 	@Test(groups = "A")
